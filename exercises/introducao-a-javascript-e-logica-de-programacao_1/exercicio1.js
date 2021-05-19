6d6cfbea4ab5196235562ff597fec118c675cbdd
Guilherme C <guilhermedc93@gmail.com>
2021-05-19 16:24:59 -0300

    realizaçao do primeiro exercicio

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio1.js
@@ -0,0 +1,14 @@
+let a = 2;
+let b = 10;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(`a soma entre ${a} e ${b} é ${adicao}`)
+console.log(`a subtração de ${a} por ${b} é ${subtracao}`)
+console.log(`a multiplicação de ${a} por ${b} é ${multiplicacao}`)
+console.log(`a a divisão de ${a} por ${b} é ${divisao}`)
+console.log(`o modulo da divisão de ${a} por ${b} é ${modulo}`)
