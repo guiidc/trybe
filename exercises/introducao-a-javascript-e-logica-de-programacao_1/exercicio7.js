977d87711940fe0c4c4876cd8b3a465039ff0701
Guilherme C <guilhermedc93@gmail.com>
2021-05-19 22:33:58 -0300

    conclui exercicio 9

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio7.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio7.js
@@ -1,4 +1,13 @@
-//Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100)
+/*
+Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100)
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F
+*/
+
 let nota = 55;
 
 if (nota >= 90) nota = 'A';
